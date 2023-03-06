6ec3d6527cc3c5aea3b87504ccfaa85b2886f2ef
Jared Williams <jaredwilliams1019@gmail.com>
2023-03-06 04:01:55 -0600

    js

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const db = require('./db/connection.js');
 const { promptUser } = require('./lib/employeeTracker.js');
 
-// Connect to the Database
+
 db.connect(err => {
     if (err) throw err;
-    console.log('Database connected.');
-    console.log('');
-    // Call function to initialize app
+    console.log('Connected Database.');
     promptUser();
 });
